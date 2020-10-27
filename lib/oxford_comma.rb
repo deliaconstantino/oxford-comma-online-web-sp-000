8d83c28d1219f663e0b12caf2f949eeda9d9cefa
Delia Constantino <dconstan92@gmail.com>
2020-10-27 21:37:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,9 @@ if array.length < 3
   string = array.join(" and ")
 else
   last_element = array.pop
-
-  puts last_element
-  print array
+  string_with_commas_only = array.join(", ")
+  final_element = ", and #{last_element}"
+  # puts last_element
+  # print array
 end
 end
