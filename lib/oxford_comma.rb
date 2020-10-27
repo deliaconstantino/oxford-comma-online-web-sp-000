7dcce8a62cf61874ad85e8292870ce0dd61463ee
Delia Constantino <dconstan92@gmail.com>
2020-10-27 21:45:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,7 @@ if array.length < 3
 else
   last_element = array.pop
   string_with_commas_only = array.join(", ")
-  final_element = ", and #{last_element}"
-  finally_print = "#{string_with_commas_only}#{final_element}"
+  final_print = "#{string_with_commas_only}, #{last_element}""
 end
 end
 
