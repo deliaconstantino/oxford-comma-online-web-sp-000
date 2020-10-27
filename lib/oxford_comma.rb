27f20ee2ab5b401bed0cf3e9f795230b0f81820f
Delia Constantino <dconstan92@gmail.com>
2020-10-27 21:46:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
-if array.length < 3
-  string = array.join(" and ")
-else
-  last_element = array.pop
-  string_with_commas_only = array.join(", ")
-  final_string = "#{string_with_commas_only}, #{last_element}""
-end
+  if array.length < 3
+    string = array.join(" and ")
+  else
+    last_element = array.pop
+    string_with_commas_only = array.join(", ")
+    final_string = "#{string_with_commas_only}, #{last_element}"
+  end
 end
 
   # puts "#{string_with_commas_conly}"
